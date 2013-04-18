a74653ce64bdab8dfe79f9f7908fde604ffc2e90
tmmaare <aare.tamm@khk.ee>
2013-04-18 08:56:06 +0300

    18.04 hommik

--- test.php
+++ test.php
@@ -1,1 +1,30 @@
-test
+<meta charset="UTF-8">
+<?php
+
+$array = array(
+	'color' => array(
+			'front_color' => 'red',
+			'back_color' => 'black'),
+	'size' => 'big',
+	'age' => 5
+);
+//echo $array['color']['back_color'];
+
+class auto {
+	public $color = 'red';
+	public $size = 'big';
+	public $age = 5;
+
+	public function käivita(){
+		echo $this->color . " auto käivitus. Prõnn!";
+	}
+}
+class veoauto extends auto {
+	public $cargo_payload_size = 500;
+}
+$auto = new veoauto;
+
+$teine_auto = new veoauto;
+$teine_auto->color = 'valge';
+$teine_auto->käivita();
+
