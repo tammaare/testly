cfd6ce8cd57f467863b313fe62345ac73f7d2c39
tmmaare <aare.tamm@khk.ee>
2013-04-18 15:24:13 +0300

    18.04 15.24

--- views/master_view.php
+++ views/master_view.php
@@ -1,8 +1,12 @@
 <!DOCTYPE html>
-<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
-<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
-<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
-<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+<!--[if lt IE 7]>
+<html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>
+<html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>
+<html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!-->
+<html class="no-js"> <!--<![endif]-->
 <head>
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
@@ -16,16 +20,52 @@
 	<link href="//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.1/css/bootstrap-combined.min.css" rel="stylesheet">
 	<link rel="stylesheet" href="assets/css/main.css">
 	<script src="assets/js/vendor/modernizr-2.6.2.min.js"></script>
+
+	<style>
+		body
+		{
+			padding-top: 60px;
+		}
+		body,html
+		{
+			background:url('<?=BASE_URL?>assets/img/back.jpg');
+			height: 100%;
+
+		}
+		table.table-bordered tr
+		{
+			background-color: #f9f9f9;
+		} ;
+	</style>
 </head>
 <body>
-<!--[if lt IE 7]>
-<p class="chromeframe">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">activate Google Chrome Frame</a> to improve your experience.</p>
-<![endif]-->
-<!-- Add your site or application content here -->
+<div class="navbar navbar-inverse navbar-fixed-top">
+	<div class="navbar-inner">
+		<div class="container">
+			<button type="button" class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+			</button>
+			<a class="brand" href="#">Project name</a>
+
+			<div class="nav-collapse collapse">
+				<ul class="nav">
+					<li class="active"><a href="<?= BASE_URL ?>tests">Home</a></li>
+					<li><a href="<?= BASE_URL ?>auth/logout">Logi välja</a></li>
+					<!-- <li><a href="<?//=BASE_URL ?>tests">Contact</a></li>-->
+				</ul>
+			</div>
+			<!--/.nav-collapse -->
+		</div>
+	</div>
+</div>
 <?php
-//nõuame views kaustast vastava kontrolleri, vastava actioni(funktsiooni) vaatefaili
-require '/views/'.$request->controller.'_'.$request->action.'_view.php';
-?>
+/*Ask controller and action attribute from my $request instance, glue it an " _view.php" ending and
+merge a view file with that name*/
+require '/views/'.$request->controller.'_'.$request->action.'_view.php';?>
+
+
 <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
 <script>window.jQuery || document.write('<script src="assets/js/vendor/jquery-1.9.1.min.js"><\/script>')</script>
 <script src="assets/js/plugins.js"></script>
--- views/tests_index_view.php
+++ views/tests_index_view.php
@@ -1,2 +1,19 @@
 
-HALLO KOSMOS!
+<p> <a class="btn btn-large btn-primary" href="https://www.google.ee">Lis uus test </a>
+</p>
+<table id="tests-table" class="table table-bordered table striped">
+	<thead>
+	<th>Testi nimi</th>
+	<th>Koostaja</th>
+	<th>Aeg</th>
+	<th>Tegevused</th>
+	</thead>
+	<tbody>
+	<tr>
+		<td>test1</td>
+		<td>buduaar</td>
+		<td>2011-09-22</td>
+		<td><?="vasta"?> <i class="icon-pencil"></i> </td>
+	</tbody>
+	</tr>
+</table>
